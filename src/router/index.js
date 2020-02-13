2ad0f9680c02ec788f3a957b8746ab7f6f20c1f2
gengjiaping <1076730460@qq.com>
2020-02-13 16:25:56 +0800

    项目配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import HelloVue from '@/components/day01/HelloVue'
 
 Vue.use(Router)
 
+export const constantRouterMap = [
+  {path: '/login', component: () => import('@/views/login/index'), hidden: true},
+  {path: '/404', component: () => import('@/views/404'), hidden: true},
+  /*{
+    path: '',
+    component: Layout,
+    redirect: '/home',
+    children: [{
+      path: 'home',
+      name: 'home',
+      component: () => import('@/views/home/index'),
+      meta: {title: '首页', icon: 'home'}
+    }]
+  },*/
+  {path: '*', redirect: '/login', hidden: true}
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/day01',
-      name: 'HelloVue',
-      component: HelloVue
-    }
-  ]
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({y: 0}),
+  routes: constantRouterMap
 })
